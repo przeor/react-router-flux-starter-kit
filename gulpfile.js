ac6520e78feebc9a1971832ad9ee2adb2c45067a
Kieran Boyle <kboyle@yahoo-inc.com>
2015-03-30 16:59:20 -0700

    include local development server task

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,58 @@
 /** @jsx React.DOM */
-var gulp = require('gulp');
-var browserify = require('gulp-browserify');
-var concat = require('gulp-concat');
+var browserify = require('gulp-browserify'),
+    concat = require('gulp-concat'),
+    connect = require('gulp-connect'),
+    gulp = require('gulp'),
+    open = require('gulp-open');
 
-gulp.task('browserify', function() {
+gulp
+  // performs magic
+  .task('browserify', function(){
     gulp.src('src/js/main.js')
-      .pipe(browserify({transform: 'reactify',debug : !gulp.env.production}))
+      .pipe(
+        browserify({ 
+          transform: 'reactify', 
+          debug: !gulp.env.production 
+        })
+      )
       .pipe(concat('main.js'))
       .pipe(gulp.dest('dist/js'));
-});
+  })
 
-gulp.task('copy', function() {
-    gulp.src('src/index.html')
+  // moves source files to dist
+  .task('copy', function(){
+    gulp
+      .src('src/index.html')
       .pipe(gulp.dest('dist'));
-	gulp.src('src/assets/**/*.*')
+	 
+     gulp
+      .src('src/assets/**/*.*')
       .pipe(gulp.dest('dist/assets'));
-});
+  })
 
-gulp.task('default',['browserify', 'copy']);
+  // local development server
+  .task('connect', function(){
+    connect.server({
+      root: ['dist'],
+      port: '8080',
+      base: 'http://localhost',
+      livereload: true
+    });
+  })  
 
-gulp.task('watch', function() {
+  // opens the application in chrome
+  .task('open', function(){
+    gulp
+      .src('dist/index.html')
+      .pipe(
+        open('', { url: 'http://localhost:8080/' })
+      );
+  })
+
+  // build the application
+  .task('default', ['browserify', 'copy', 'connect', 'open'])
+  
+  // watch for source changes
+  .task('watch', function(){
     gulp.watch('src/**/*.*', ['default']);
-});
+  });
