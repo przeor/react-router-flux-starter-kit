787f0a9b01ad219b4f20a10f124564eb62fe1f97
Kamil Przeorski <kamil@przeorski.pl>
2015-04-17 11:10:14 +0200

    app-auth actions improvement

--- src/js/actions/app-auth.js
+++ src/js/actions/app-auth.js
@@ -1,15 +1,14 @@
 var AppConstants = require('../constants/app-constants');
 var AppDispatcher = require('../dispatchers/app-dispatcher');
 
-function _dispatch(key, email, pass) {
-    var payload = {actionType: key, email: email, pass:pass};
-    AppDispatcher.handleRequestAction(payload);
-}
-
-
 var AuthActions = {
     startAuth: function(email, pass) {
-    	_dispatch(AppConstants.AUTH_LOG_IN, email, pass)
+    var payload = {
+    	'actionType': AppConstants.AUTH_LOG_IN, 
+    	'email': email, 
+    	'pass': pass
+    };
+    AppDispatcher.handleViewAction(payload)
     },
 }
 
