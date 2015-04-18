b55e7aede95071f542c97ec0a9b76d3f1df093f2
Kamil Przeorski <kamil@przeorski.pl>
2015-04-18 15:16:25 +0200

    added gulp live reload with plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
-/** @jsx React.DOM */
 var browserify = require('gulp-browserify'),
     concat = require('gulp-concat'),
     connect = require('gulp-connect'),
     gulp = require('gulp'),
-    open = require('gulp-open');
-
+    open = require('gulp-open'),
+    plumber = require('gulp-plumber'),
+    livereload = require('gulp-livereload');
+ 
 gulp
   // performs magic
   .task('browserify', function(){
     gulp.src('src/js/main.js')
+      .pipe(plumber())
       .pipe(
         browserify({ 
           transform: 'reactify', 
@@ -16,20 +18,26 @@ gulp
         })
       )
       .pipe(concat('main.js'))
-      .pipe(gulp.dest('dist/js'));
+      .pipe(plumber.stop())
+      .pipe(gulp.dest('dist/js'))
+      .pipe(livereload());
   })
-
+ 
   // moves source files to dist
   .task('copy', function(){
     gulp
       .src('src/index.html')
       .pipe(gulp.dest('dist'));
-	 
+   
      gulp
       .src('src/assets/**/*.*')
       .pipe(gulp.dest('dist/assets'));
+   
+     gulp
+      .src('src/img/**/*.*')
+      .pipe(gulp.dest('dist/img'));
   })
-
+ 
   // local development server
   .task('connect', function(){
     connect.server({
@@ -39,20 +47,23 @@ gulp
       livereload: true
     });
   })  
-
+ 
   // opens the application in chrome
   .task('open', function(){
     gulp
       .src('dist/index.html')
       .pipe(
-        open('', { url: 'http://localhost:8080/' })
+        open('', {app: 'google chrome',url: 'http://localhost:8080/'})
       );
   })
-
+ 
+ 
   // build the application
   .task('default', ['browserify', 'copy', 'connect', 'open'])
+  .task('default2', ['browserify', 'copy'])
   
   // watch for source changes
-  .task('watch', function(){
-    gulp.watch('src/**/*.*', ['default']);
+  .task('watch', ['default'], function(){
+    livereload.listen();
+    gulp.watch('src/**/*.*', ['default2']);
   });
